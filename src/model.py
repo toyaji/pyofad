1282b0f2ed7409c34390351ae4f2c520cf769d84
PaulShin83 <toyaji@boodong.kr>
2023-01-13 02:32:42 +0000

    add basde document model scratch

--- src/model.py
+++ src/model.py
@@ -1,1 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PrivateAttr
+from typing import Any
+from .orm.orm_base import ORMBase
+from typing import TypeVar, ClassVar, Generic, Self
+
+class FADModel(BaseModel, ORMBase):
+    _table: str = PrivateAttr()
+    _caller_chain: str | None = PrivateAttr()
+
+    def __init__(__pydantic_self__, table=None, caller_chain=None, **data: Any) -> None:
+        super().__init__(**data)
+        __pydantic_self__._table = table
+        __pydantic_self__._caller_chain = caller_chain
+
+
+class TestDocument(FADModel):
+    a: str
+    b: int
